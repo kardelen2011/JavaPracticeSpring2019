b600964a65e196ba0e6096571f82a5319be42df8
kardelen2011 <bahadirkamil20@gmail.com>
2019-09-09 23:52:21 -0400

    second commit

--- src/KamilPackage/HelloWorld.java
+++ src/KamilPackage/HelloWorld.java
@@ -4,7 +4,11 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello World");
+		System.out.println("kjsdfkjsd");
+		System.out.println("fkjsadjksdfjkhfsda");
+		
+		
 	}
 
 }
